3bbb4ef0922891b39c3218d92cc46af1e89c52fa
Shayan7860 <Shayan.3958@gmail.com>
2023-10-18 12:08:25 -0400

    Firbase authentication - Shayan

--- App.js
+++ App.js
@@ -11,6 +11,28 @@ import Category2 from './Screens/Category2';
 import LoadingScreen from './Screens/LoadingScreen';
 import ProfilePage from './Screens/ProfilePage';
 
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/database';
+
+
+// Initialize Firebase
+// Make sure to replace the configuration with your Firebase project's details
+const firebaseConfig = {
+  apiKey: "AIzaSyA-1_M8ZRIe6N-AgWKZwWsgtLOmVnLApjQ",
+  authDomain: "circlee-a4b5d.firebaseapp.com",
+  databaseURL: "https://circlee-a4b5d-default-rtdb.firebaseio.com/",
+  projectId: "circlee-a4b5d",
+  storageBucket: "circlee-a4b5d.appspot.com",
+  messagingSenderId: "361381373341",
+  appId: "1:361381373341:web:0838b1f671b92f56d2bb74",
+  measurementId: "G-NGWS4NW9QB"
+  
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 const Stack = createStackNavigator();
 
 export default function App() {
