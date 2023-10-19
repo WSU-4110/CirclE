009d7bc21ee5788c7c914de9fa55d1b69a204a42
zjbaby97 <zhanjia920907@gmail.com>
2023-10-19 16:36:43 -0400

    catergories1

--- Screens/Category1.js
+++ Screens/Category1.js
@@ -1,13 +1,66 @@
-// Category1.js
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  VirtualizedList,
+  StyleSheet,
+  Text,
+  StatusBar,
+  Image
+} from 'react-native';
+
+
+const getItem = (_data, index) => ({
+  id: Math.random().toString(12).substring(0),
+  title: `Item ${index + 1}`,
+});
+
+const getItemCount = _data => 12;
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+    <Image
+        style={styles.tinyLogo}
+        source={require('../assets/dog.jpeg')}
+      />
+  </View>
+);
 
 const Category1 = () => {
   return (
-    <View>
-      <Text>Category 1 Page</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <VirtualizedList
+        initialNumToRender={4}
+        renderItem={({item}) => <Item title={item.title} />}
+        keyExtractor={item => item.id}
+        getItemCount={getItemCount}
+        getItem={getItem}
+      />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  tinyLogo: {
+    width: 300,
+    height: 100,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    height: 150,
+    justifyContent: 'center',
+    marginVertical: 8,
+    marginHorizontal: 16,
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
 
 export default Category1;
