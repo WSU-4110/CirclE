8b57f5f40e8e3d536c44577d816195eb4f9a88cc
Ahmad Qureshi <m.ahmad.qureshi1217@gmail.com>
2023-10-24 11:09:33 -0400

    Added categories to user items

--- Screens/userdefineditems.js
+++ Screens/userdefineditems.js
@@ -1,25 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, FlatList } from 'react-native';
+import { View, Text, TextInput, StyleSheet, FlatList, Alert, ScrollView, TouchableOpacity } from 'react-native';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/database';
-import { Alert } from 'react-native';  // Add this import at the top
-import { TouchableOpacity } from 'react-native';  // Add TouchableOpacity
-
 
 const UserDefinedItems = () => {
   const [newItem, setNewItem] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('General');
   const [items, setItems] = useState([]);
-
-  // Reference to your Firebase database
   const itemsRef = firebase.database().ref('userDefinedItems');
 
-  // Function to add item to Firebase
   const handleAddItem = () => {
     if (newItem.trim() !== '') {
       const newItemRef = itemsRef.push();
       newItemRef.set({
         id: newItemRef.key,
-        name: newItem
+        name: newItem,
+        category: selectedCategory
       });
       setNewItem('');
     }
@@ -62,13 +58,29 @@ const UserDefinedItems = () => {
     <View style={styles.container}>
       <View style={styles.card}>
         <Text style={styles.title}>User Defined Items</Text>
+
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {['General', 'Electronics', 'Clothing', 'Food'].map((category) => (
+            <TouchableOpacity
+              key={category}
+              style={[styles.categoryButton, selectedCategory === category && styles.selectedCategory]}
+              onPress={() => setSelectedCategory(category)}
+            >
+              <Text style={styles.categoryText}>{category}</Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+
         <TextInput
           style={styles.input}
           placeholder="Add new item..."
           value={newItem}
           onChangeText={(text) => setNewItem(text)}
         />
-        <Button title="Add Item" onPress={handleAddItem} />
+
+<TouchableOpacity style={styles.addButton} onPress={handleAddItem}>
+  <Text style={styles.addButtonText}>Add Item</Text>
+</TouchableOpacity>
         <FlatList
   data={items}
   keyExtractor={(item) => item.id.toString()}
@@ -116,24 +128,90 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     width: '100%',
   },
-    listItem: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      backgroundColor: '#F1F1F1',
-      padding: 15,
-      marginVertical: 5,
-      borderRadius: 5,
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#F1F1F1',
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 5,
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',  // Dark gray
+  },
+  deleteButtonOutside: {
+    position: 'absolute',
+    right: 10,
+    top: 10,
+    padding: 10,
+    borderRadius: 20,
+    backgroundColor: '#F44336',  // Red
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  deleteButtonText: {
+    color: '#FFF',  // White
+  },
+    // Add Item Button
+  addButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    backgroundColor: '#2196F3',  
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  addButtonText: {
+    fontSize: 16,
+    color: '#FFF',  // White
+  },
+    // Delete Button
+  deleteButton: {
+    padding: 10,
+    borderRadius: 50,
+    backgroundColor: '#F44336',  
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  deleteButtonText: {
+    color: '#FFF',  
     },
-    deleteButton: {
-      backgroundColor: '#FF0000',
-      padding: 10,
-      borderRadius: 5,
-    },
-    deleteButtonText: {
-      color: '#FFFFFF',
-    },
-  });
-  
+    // Category Buttons
+  categoryButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    margin: 5,
+    borderRadius: 20,
+    backgroundColor: '#E0E0E0',  
+    elevation: 3,  
+    shadowColor: '#000',  
+    shadowOffset: { width: 0, height: 2 },  
+    shadowOpacity: 0.2,  
+    shadowRadius: 3,  
+  },
+  selectedCategory: {
+    backgroundColor: '#4CAF50',  
+  },
+  categoryText: {
+    fontSize: 16,
+    color: '#333', 
+  },
+  selectedCategoryText: {
+    color: '#FFF', 
+  },
+    });
+    
 
 export default UserDefinedItems;
