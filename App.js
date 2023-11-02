443f95de97820eca7c575d29182959d5f1916076
niniorithemofe <hd9510@wayne.edu>
2023-11-02 15:45:28 -0400

    "merging changes to master branch from mock-databse-nini

--- App.js
+++ App.js
@@ -38,7 +38,21 @@ import 'firebase/compat/database';
 // Initialize Firebase
 // Make sure to replace the configuration with your Firebase project's details
 
+const firebaseConfig = {
+  apiKey: "AIzaSyA-1_M8ZRIe6N-AgWKZwWsgtLOmVnLApjQ",
+  authDomain: "circlee-a4b5d.firebaseapp.com",
+  databaseURL: "https://circlee-a4b5d-default-rtdb.firebaseio.com/",
+  projectId: "circlee-a4b5d",
+  storageBucket: "circlee-a4b5d.appspot.com",
+  messagingSenderId: "361381373341",
+  appId: "1:361381373341:web:0838b1f671b92f56d2bb74",
+  measurementId: "G-NGWS4NW9QB"
+  
+};
 
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
