4be124139bade57f320f6463797436e44aa48ff7
Shayan7860 <Shayan.3958@gmail.com>
2023-11-13 23:17:15 -0500

    unit testing updated

--- Screens/Login.test.js
+++ Screens/Login.test.js
@@ -1,9 +1,14 @@
 import { render, fireEvent } from '@testing-library/react-native';
 import { useNavigation } from '@react-navigation/native';
 import Login from './Login';
+import { Alert } from 'react-native';
 
 jest.mock('@react-navigation/native', () => ({
   useNavigation: jest.fn(),
+}));
+
+jest.mock('react-native/Libraries/Alert/Alert', () => ({
+  alert: jest.fn(),
 }));
 
 describe('Login', () => {
@@ -33,7 +38,6 @@ describe('Login', () => {
     fireEvent.press(loginButton);
     expect(navigate).toHaveBeenCalledWith('Home');
   });
-  
 
   it('navigates to CreateAccount on button press', () => {
     const navigate = jest.fn();
@@ -42,5 +46,24 @@ describe('Login', () => {
     const createAccountButton = getByText("Don't have an account? Sign Up");
     fireEvent.press(createAccountButton);
     expect(navigate).toHaveBeenCalledWith('CreateAccount');
+  });
+
+  it('displays error message on incorrect login', () => {
+    const { getByPlaceholderText, getByText, getAllByText } = render(<Login />);
+    const usernameInput = getByPlaceholderText('Username');
+    const passwordInput = getByPlaceholderText('Password');
+    fireEvent.changeText(usernameInput, 'wronguser');
+    fireEvent.changeText(passwordInput, 'wrongpassword');
+    const loginButton = getAllByText('Login')[1];
+    fireEvent.press(loginButton);
+    const errorMessage = getByText('Invalid username or password');
+    expect(errorMessage).toBeTruthy();
+  });
+
+  it('displays alert on Forgot Password press', () => {
+    const { getByText } = render(<Login />);
+    const forgotPasswordLink = getByText('Forgot Password?');
+    fireEvent.press(forgotPasswordLink);
+    expect(Alert.alert).toHaveBeenCalledWith('Forgot Password', 'Redirecting to forgot password screen.');
   });
 });
